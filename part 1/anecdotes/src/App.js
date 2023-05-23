9543f85c7a9dc9844d735fc061f716eb4302bfaf
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2023-05-23 22:44:28 +0300

    finished the anecdote task

--- part 1/anecdotes/src/App.js
+++ part 1/anecdotes/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const MaximumVote = (props) => {
+  const maxVote = Math.max(...props.voteCounts);
+  const maxIndex = props.voteCounts.indexOf(maxVote);
+
+  if (maxVote === 0) {
+    return <p>No votes yet</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[maxIndex]}</p>
+    </>
+  );
+};
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [voteCounts, setVoteCounts] = useState(new Array(6).fill(0));
+
+  const handleVote = (selected) => {
+    const updatedVotes = [...voteCounts];
+    updatedVotes[selected] += 1;
+    setVoteCounts(updatedVotes);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>Has {voteCounts[selected]} votes</p>
+      <p>
+        <button onClick={() => handleVote(selected)}>Vote</button>
+        <button onClick={() => setSelected(Math.floor(Math.random() * 6))}>Next Anecdote</button>
+      </p>
+      <MaximumVote voteCounts={voteCounts} anecdotes={props.anecdotes} />
     </div>
   );
-}
+};
 
 export default App;
