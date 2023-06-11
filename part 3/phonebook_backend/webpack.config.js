a04fe38b5cd9bdf78658c9e2ac3a0715790991c7
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2023-06-11 16:45:53 +0200

    unncessessary fallback

--- part 3/phonebook_backend/webpack.config.js
+++ part 3/phonebook_backend/webpack.config.js
@@ -2,11 +2,5 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
     plugins: [
         new Dotenv()
-    ],
-    resolve: {
-        fallback: {
-          net: false,
-          // add fallbacks for other Node.js core modules as necessary
-        }
-    }
+    ]
 }
