8db13c433731ea565f044892f3d8cdad285317ff
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2023-06-12 22:09:12 +0200

    some changes

--- part 4/blog_list/tests/array.test.js
+++ part 4/blog_list/tests/array.test.js
@@ -35,14 +35,6 @@ describe('total likes', () => {
           __v: 0
         },
         {
-          _id: "5a422b891b54a676234d17fa",
-          title: "First class tests",
-          author: "Robert C. Martin",
-          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-          likes: 10,
-          __v: 0
-        },
-        {
           _id: "5a422ba71b54a676234d17fb",
           title: "TDD harms architecture",
           author: "Robert C. Martin",
@@ -73,30 +65,6 @@ describe('favorite blog', () => {
           author: "Michael Chan",
           url: "https://reactpatterns.com/",
           likes: 7,
-          __v: 0
-        },
-        {
-          _id: "5a422aa71b54a676234d17f8",
-          title: "Go To Statement Considered Harmful",
-          author: "Edsger W. Dijkstra",
-          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-          likes: 5,
-          __v: 0
-        },
-        {
-          _id: "5a422b3a1b54a676234d17f9",
-          title: "Canonical string reduction",
-          author: "Edsger W. Dijkstra",
-          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-          likes: 12,
-          __v: 0
-        },
-        {
-          _id: "5a422b891b54a676234d17fa",
-          title: "First class tests",
-          author: "Robert C. Martin",
-          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-          likes: 10,
           __v: 0
         },
         {
@@ -166,14 +134,6 @@ describe('most frequent blogger', () => {
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
       likes: 0,
       __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
     }]
 
     test('find most frequent', () => {
@@ -200,38 +160,6 @@ describe('most liked blogger', () => {
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
       __v: 0
     }]
 
